be7fa4cdde2cb45e2420fe4e4c1eb46972356c71
João Trindade <jtrindade@cc.isel.ipl.pt>
2017-04-17 21:34:32 +0100

    Aula de 2017-04-17 : Parte I

---
+++ 2017-04-17/log/UseLog/UseLog.cs
@@ -0,0 +1,63 @@
+using System;
+
+public class Info 
+{
+	public int a;
+
+	public int b;
+
+	[Level(3)]
+	public int c;
+
+	[DontLog]
+	public int d;
+	
+	public Info(int a, int b, int c, int d) 
+	{
+		this.a = a; this.b = b; this.c = c; this.d = d;
+	}
+}
+
+public class User
+{
+	[Level(1)]
+	private string username;
+	
+	[DontLog]
+	private string password;
+	
+	private string name;
+	
+	public User(string uname, string passwd, string fullname) {
+		username = uname; password = passwd; name = fullname;
+	}
+}
+
+public class Logs 
+{
+	static readonly ILog TheRefLog = new RefLog();
+	
+	static ILog GetLogger(object obj)
+	{
+		// return TheRefLog;
+		// OR
+		return GenLog.For(obj.GetType().GetTypeInfo());
+	}
+
+	static void Log(object obj, int level)
+	{
+		GetLogger(obj).Log(obj, level);
+	}
+	
+	public static void Main()
+	{
+		Info info = new Info(1, 2, 3, 4);
+		User user = new User("jtrindade", "1234", "Joao Trindade");
+
+		Log(info, 2);
+		Log(info, 5);
+
+		Log(user, 0);
+		Log(user, 2);
+	}
+}
