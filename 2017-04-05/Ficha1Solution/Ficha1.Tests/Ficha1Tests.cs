718d83ca46116450cfe6985e7ce52bf18e738189
João Trindade <jtrindade@cc.isel.ipl.pt>
2017-04-22 19:54:43 +0100

    Aula de 2017-04-05

---
+++ 2017-04-05/Ficha1Solution/Ficha1.Tests/Ficha1Tests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AVE
+{
+	[TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestEx1WithAReferenceType() 
+        {
+            // Arrange
+            string x = "AVE";
+            
+            // Act
+            bool res = Ficha1.IsReferenceEqualsToSelf(x);
+            
+            // Assert
+            Assert.That(res, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void TestEx1WithAValueType() 
+        {
+            // Arrange
+            int x = 20;
+            
+            // Act
+            bool res = Ficha1.IsReferenceEqualsToSelf(x);
+            
+            // Assert
+            Assert.That(res, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void TestEx2WithA() 
+        {
+            // Arrange
+            A obj = new A();
+            
+            // Act
+            bool res = Ficha1.ConfirmIsTypeA(obj);
+            
+            // Assert
+            Assert.That(res, Is.EqualTo(true));
+        }
+        
+        public class B : A {}
+
+        [Test]
+        public void TestEx2WithDerivedFromA() 
+        {
+            // Arrange
+            A obj = new B();
+            
+            // Act
+            bool res = Ficha1.ConfirmIsTypeA(obj);
+            
+            // Assert
+            Assert.That(res, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void TestEx5() 
+        {
+            // Arrange
+            string[] expectedMethodNames = new string[] {
+					"ToString", "Equals", "GetHashCode", "GetType",
+					"get_Count", "set_Count", "None"
+				};
+            
+            // Act
+            string[] methodNames = Ficha1.GetMethodsOfX();
+            
+            // Assert
+            Assert.That(methodNames, Is.EquivalentTo(expectedMethodNames));
+        }
+        
+        public class Dst
+        {
+			public int Val { get; set; }
+		}
+		
+		public class Src
+		{
+			public int item;
+		}
+        
+        [Test]
+        public void TestEx6WithInvalidPropertyName() 
+        {
+			Dst dst = new Dst();
+			Src src = new Src();
+			
+			bool res = Ficha1.SetPropertyFromField(dst, "NonExistingProperty", src, "item");
+			
+			Assert.That(res, Is.EqualTo(false));
+		}
+    }
+}
---
+++ 2017-04-05/Ficha1Solution/Ficha1/Ficha1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+
+namespace AVE
+{
+    public class A {}
+
+    public class Ficha1
+    {
+        public static bool IsReferenceEqualsToSelf(string x)
+        {    
+            return Object.ReferenceEquals(x, x);
+        }
+
+        public static bool IsReferenceEqualsToSelf(int x)
+        {    
+            return Object.ReferenceEquals(x, x);
+        }
+
+        public static bool ConfirmIsTypeA(A obj)
+        {    
+            return obj.GetType() == typeof(A);
+        }
+
+        public class X
+        {
+            private int count;
+            public int Count
+            {
+                get { return count; }
+                set { count = value; }
+            }
+            public static void None() {}
+        }
+        
+        public static string[] GetMethodsOfX() 
+        {
+            MethodInfo[] methods = typeof(X).GetTypeInfo().GetMethods();
+            
+            string[] methodNames = new string[methods.Length];
+            for (int i = 0; i < methods.Length; ++i) {
+                methodNames[i] = methods[i].Name;
+            }
+
+            return methodNames;
+        }
+
+        public static bool SetPropertyFromField(object dst, string dstProp, object src, string srcFld)
+        {
+			// TO DO
+            return false;
+        }
+    }
+}
